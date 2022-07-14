7505ab0762ea2997833eae4368650434b8660d9d
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2022-07-15 00:04:27 +0200

    VSProjectFiles + Common.Naming

---
+++ CoffeeBackup/CoffeeBackup.Service/Program.cs
@@ -0,0 +1,10 @@
+using CoffeeBackup.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
